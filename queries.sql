ff1f9799e9e555b231c8cbb6a27bf42cf60956ed
dqarias <dino.quispea@gmail.com>
2022-08-10 13:08:48 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -15,27 +15,39 @@ SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
 -- Transaction 1 
 BEGIN;
 UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 -- Transaction 2
 BEGIN;
 UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
 UPDATE animals SET species='pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
 COMMIT;
+SELECT * FROM animals;
 
 -- Transaction 3
 BEGIN;
 DELETE FROM animals;
+SELECT COUNT(*) FROM ANIMALS;
 ROLLBACK;
+SELECT COUNT(*) FROM ANIMALS;
 
 -- Transaction 4
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT SP1;
+SELECT * FROM animals;
 UPDATE animals SET weight_kg = weight_kg * -1;
+SELECT * FROM animals;
 ROLLBACK TO SP1;
+SELECT * FROM animals;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
 COMMIT;
+SELECT * FROM animals;
 
 -- Queries
 
@@ -50,4 +62,4 @@ SELECT SUM(escape_attempts), neutered FROM animals GROUP BY neutered;
 -- What is the minimum and maximum weight of each type of animal?
 SELECT MIN(weight_kg), MAX(weight_kg), species from animals GROUP BY species;
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth >= '1990/01/01' AND date_of_birth <= '2000/12/31' GROUP BY species;
